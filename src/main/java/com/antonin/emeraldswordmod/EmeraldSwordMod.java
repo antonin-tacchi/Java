bf68fd554934fcebfa338c79bdfc2409907a827b
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-07-18 13:49:01 +0200

    modifi duration of effect

--- src/main/java/com/antonin/emeraldswordmod/EmeraldSwordMod.java
+++ src/main/java/com/antonin/emeraldswordmod/EmeraldSwordMod.java
@@ -1,28 +1,21 @@
 package com.antonin.emeraldswordmod;
 
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.*;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.*;
 import net.minecraft.world.level.Level;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.sounds.*;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import net.minecraftforge.event.*;
+import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.*;
+
 
 @Mod(EmeraldSwordMod.MODID)
 public class EmeraldSwordMod {
@@ -203,7 +196,7 @@ public class EmeraldSwordMod {
                 @Override
                 public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
                     if (attacker instanceof Player player) {
-                        player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 200, 0));
+                        player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 2000, 0));
                         Level level = player.level();
                         if (level instanceof ServerLevel serverLevel) {
                             serverLevel.sendParticles(ParticleTypes.END_ROD,
